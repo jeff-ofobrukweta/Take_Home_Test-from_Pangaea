2a70f97a9a27be3177403fdb7453370946998eeb
Ofobrukweta jeff <oghenerukevwejeff@gmail.com>
2021-03-26 03:20:36 +0100

    chore : added the header cart icon

--- src/components/headerComponent/index.vue
+++ src/components/headerComponent/index.vue
@@ -1,17 +1,29 @@
 <template>
-  <nav class="header-main">
+  <nav class="header-main navigation-container">
     <div class="header-main-left">1</div>
-    <div class="header-main-right">2</div>
+    <div class="header-main-right">
+      <div class="cart-container">
+        <img :src="Cart" alt="shoping cart" class="cart-icon"/>
+        <span class="cart-number">0</span>
+      </div>
+      <div class="select-container"></div>
+    </div>
   </nav>
 </template>
 
 <script>
+import Cart from "../../assets/images/icons/cart.png";
+
 export default {
   name: "HeaderComponent",
-
+  data() {
+    return {
+      Cart,
+    };
+  },
   components: {},
 
-  props: {}
+  props: {},
 };
 </script>
 
--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home-component">
-    <ButtonComponent />
+    <ButtonComponent customName="btn" />
   </div>
 </template>
 
